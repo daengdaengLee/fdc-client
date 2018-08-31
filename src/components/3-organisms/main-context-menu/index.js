7e1842ecd309acbda6141d34376ea8f19a18d8d4
daengdaenglee <gunho1020@gmail.com>
2018-08-31 15:24:34 +0900

    ENH: Make main context menu component

--- src/components/3-organisms/main-context-menu/index.js
+++ src/components/3-organisms/main-context-menu/index.js
@@ -1,3 +1,20 @@
+import { connect } from 'react-redux';
+import { close, clickMenu } from '../../../ducks/modules/context-menus';
 import Presenter from './presenter';
 
-export default Presenter;
+const mapStateToProps = state => ({
+  onOff: state.contextMenus.onOff,
+  x: state.contextMenus.x,
+  y: state.contextMenus.y,
+  theme: state.contextMenus.theme,
+});
+
+const mapDispatchToProps = dispatch => ({
+  onClickMenu: ({ event, item }) => dispatch(clickMenu({ event, item })),
+  onClickOutside: () => dispatch(close()),
+});
+
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps,
+)(Presenter);
--- src/components/3-organisms/main-context-menu/presenter.js
+++ src/components/3-organisms/main-context-menu/presenter.js
@@ -1,5 +1,53 @@
 import React from 'react';
+import ContextMenu from '../../2-molecules/context-menu';
 
-const MainContextMenu = () => null;
+const calcWidth = theme => {
+  switch (theme) {
+  case 'table':
+    return '300px';
+  case 'tree':
+    return '200px';
+  default:
+    return '300px';
+  }
+};
+
+const selectItems = theme => {
+  switch (theme) {
+  case 'table':
+    return [
+      {
+        name: 'View Trace Data(Time)',
+        icon: 'area-chart',
+        key: 'TRACE_TIME',
+      },
+      { name: 'View Trace Data(Lot)', icon: 'pie-chart', key: 'TRACE_LOT' },
+      {
+        name: 'View Trace Data(Overlay)',
+        icon: 'dot-chart',
+        key: 'TRACE_OVERLAY',
+      },
+    ];
+  case 'tree':
+    return [
+      { name: 'Real Time View', icon: 'star-o', key: 'VIEW_REAL_TIME' },
+      { name: 'Lot/Wafer View', icon: 'star-o', key: 'VIEW_LOT_WAFER' },
+    ];
+  default:
+    return [];
+  }
+};
+
+const MainContextMenu = ({ onOff, x, y, theme, onClickMenu, onClickOutside }) =>
+  onOff && (
+    <ContextMenu
+      width={calcWidth(theme)}
+      items={selectItems(theme)}
+      x={x}
+      y={y}
+      onClickMenu={onClickMenu}
+      onClickOutside={onClickOutside}
+    />
+  );
 
 export default MainContextMenu;
