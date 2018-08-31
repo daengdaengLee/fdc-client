82fb3b670e094e58e6c9849d4401a53ebaa8a7fa
daengdaenglee <gunho1020@gmail.com>
2018-08-31 13:57:56 +0900

    ENH: Init parameters saga

--- src/ducks/sagas/index.js
+++ src/ducks/sagas/index.js
@@ -1,7 +1,8 @@
 import { all } from 'redux-saga/effects';
 import historiesSaga from './histories';
 import treesSaga from './trees';
+import parametersSaga from './parameters';
 
 export default function* rootSaga() {
-  yield all([historiesSaga(), treesSaga()]);
+  yield all([historiesSaga(), treesSaga(), parametersSaga()]);
 }
---
+++ src/ducks/sagas/parameters.js
@@ -0,0 +1,38 @@
+import { all, takeEvery, select, call, put } from 'redux-saga/effects';
+import {
+  REQUEST_FETCH,
+  FETCH_START,
+  fetchStart,
+  fetchSuccess,
+  fetchFail,
+  setParams,
+} from '../modules/parameters';
+import { getTree } from '../../assets/js/requests';
+
+// Helpers
+
+// Workers
+function* requestFetchSaga({ fab, eqp, from, to, lot }) {
+  const { isLoading } = yield select(state => state.trees);
+  yield isLoading || put(fetchStart({ fab, eqp, from, to, lot }));
+}
+
+function* fetchStartSaga({ fab, eqp, from, to, lot }) {
+  const { data, success } = yield call(getTree, fab, eqp, from, to, lot);
+  yield put(success ? fetchSuccess() : fetchFail());
+  const dummyData = [];
+  yield put(setParams({ nodes: dummyData }));
+}
+
+// Watchers
+function* watchRequestFetch() {
+  yield takeEvery(REQUEST_FETCH, requestFetchSaga);
+}
+
+function* watchFetchStart() {
+  yield takeEvery(FETCH_START, fetchStartSaga);
+}
+
+export default function* parametersSaga() {
+  yield all([watchRequestFetch(), watchFetchStart()]);
+}
