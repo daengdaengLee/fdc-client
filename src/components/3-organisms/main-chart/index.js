11e9a0bb3cd40d15c3d70cae5d89f58ee5741eca
daengdaenglee <gunho1020@gmail.com>
2018-09-07 16:11:38 +0900

    BUG: Change mockup date to real data

--- src/components/3-organisms/main-chart/index.js
+++ src/components/3-organisms/main-chart/index.js
@@ -2,15 +2,20 @@ import { connect } from 'react-redux';
 import { setSelected } from '../../../ducks/modules/parameters';
 import Presenter from './presenter';
 
-const mapStateToProps = state => ({
-  parameters: state.parameters.params,
-  selectedParams: state.parameters.selected,
-  fab: 'M14',
-  mod: 'MODULE_1',
-  from: state.dates.from,
-  to: state.dates.to,
-  lot: 'LOT_1',
-});
+const mapStateToProps = state => {
+  const selectedHistory = state.histories.rows.find(
+    row => row.key === state.histories.selectedRowKeys[0],
+  );
+  return {
+    parameters: state.parameters.params,
+    selectedParams: state.parameters.selected,
+    fab: state.trees.fab,
+    mod: state.trees.selected[0],
+    from: state.dates.from,
+    to: state.dates.to,
+    lot: selectedHistory ? selectedHistory.LOT_ID : '',
+  };
+};
 
 const mapDispatchToProps = dispatch => ({
   onClickParam: param => dispatch(setSelected({ selected: [param] })),
