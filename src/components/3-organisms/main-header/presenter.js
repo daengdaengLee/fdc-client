d6dc91293d88d8556694fcfa5587443eb9c138d9
daengdaenglee <gunho1020@gmail.com>
2018-09-12 13:45:22 +0900

    ENH: Change tab design

--- src/components/3-organisms/main-header/presenter.js
+++ src/components/3-organisms/main-header/presenter.js
@@ -1,23 +1,24 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import styled from 'styled-components';
-import { Breadcrumb } from 'antd';
 
 const Container = styled.div`
   width: 100%;
   height: 100%;
-  padding: 0 10px;
-  background-color: #fff;
+  background-color: #ffffff;
   border-bottom: 1px solid #e3e3e3;
   display: flex;
-  align-items: center;
 `;
 
 const Link = styled.div`
   cursor: pointer;
-  display: inline-flex;
+  width: 200px;
+  display: flex;
   align-items: center;
-  color: ${props => (props.active ? '#04bed6' : '#000000')};
+  justify-content: center;
+  box-sizing: border-box;
+  border-top: ${props =>
+    props.active ? '3px solid #04bed6' : '3px solid #ffffff'};
 `;
 
 class MainHeader extends Component {
@@ -25,18 +26,12 @@ class MainHeader extends Component {
     const { location, onClickHistory, onClickTrace } = this.props;
     return (
       <Container>
-        <Breadcrumb separator="||">
-          <Breadcrumb.Item>
-            <Link onClick={onClickHistory} active={location === 'histories'}>
-              Lot / Wafer History View
-            </Link>
-          </Breadcrumb.Item>
-          <Breadcrumb.Item>
-            <Link onClick={onClickTrace} active={location === 'charts'}>
-              Trace Data View
-            </Link>
-          </Breadcrumb.Item>
-        </Breadcrumb>
+        <Link onClick={onClickHistory} active={location === 'histories'}>
+          Lot / Wafer History View
+        </Link>
+        <Link onClick={onClickTrace} active={location === 'charts'}>
+          Trace Data View
+        </Link>
       </Container>
     );
   }
