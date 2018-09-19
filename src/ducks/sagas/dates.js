a30c9155ed20452dd61714f1f71220dceb1cd519
daengdaenglee <gunho1020@gmail.com>
2018-09-19 17:23:51 +0900

    ENH: Validate if user select future date

--- src/ducks/sagas/dates.js
+++ src/ducks/sagas/dates.js
@@ -2,6 +2,7 @@ import { takeEvery, all, put, select } from 'redux-saga/effects';
 import { push } from '../modules/routes';
 import { SELECT_FROM, SELECT_TO, setFrom, setTo } from '../modules/dates';
 import { setBy } from '../modules/histories';
+import { getDateString, notiError } from '../../assets/js/utils';
 
 // Workers
 function* selectFromSaga({ date }) {
@@ -12,6 +13,11 @@ function* selectFromSaga({ date }) {
 }
 
 function* selectToSaga({ date }) {
+  const selectedTimestamp = new Date(date).getTime();
+  const today = getDateString();
+  const todayTimestamp = new Date(today).getTime();
+  if (selectedTimestamp > todayTimestamp)
+    return notiError('Cannot select tomorrow!');
   const { by } = yield select(state => state.histories);
   yield put(setTo({ date }));
   yield put(push({ location: 'main' }));
